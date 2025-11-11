c6b8122fdf4ab050a1370c354dcd266e4e69128c
ucn0925 <195886413+ucn0925@users.noreply.github.com>
2025-11-11 16:15:14 +0900

    フォロー中、フォロワー一覧ページを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,15 @@ class UsersController < ApplicationController
     @favorite_shops = @user.favorite_shops
     @posts = @user.posts.includes(:shop)
   end
+
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.followings
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+  end
+  
 end
