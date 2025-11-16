858e67a81f17ce634fa4617cad8231e198991ca6
ucn0925 <195886413+ucn0925@users.noreply.github.com>
2025-11-16 14:35:35 +0900

    プロフィール編集で名前を編集できるようにした

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,7 +22,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:profile, :profile_image, dog_attributes: [:name, :breed, :size, :dog_image]])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:profile, :profile_image, dog_attributes: [:name, :breed, :size, :dog_image]])
+        devise_parameter_sanitizer.permit(
+            :sign_up, keys: [
+                :name,
+                :profile, 
+                :profile_image, 
+                dog_attributes: [:name, :breed, :size, :dog_image]
+            ]
+        )
+        devise_parameter_sanitizer.permit(
+            :account_update, keys: [
+                :name,
+                :profile, 
+                :profile_image, 
+                dog_attributes: [:name, :breed, :size, :dog_image]
+            ]
+        )
     end
 end
