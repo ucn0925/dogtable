51284fa947443a774eb46ef7c07a737b7aada767
ucn0925 <195886413+ucn0925@users.noreply.github.com>
2025-11-19 11:42:06 +0900

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'homes#top'
   
   devise_for :users, controllers: {
     registrations: 'users/registrations'
@@ -33,7 +34,7 @@ Rails.application.routes.draw do
   get "timeline", to: "posts#timeline"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "shops#index"
+  #root "shops#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
