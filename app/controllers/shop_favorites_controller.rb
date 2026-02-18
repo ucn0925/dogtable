dbadfac216e69d61eaa126ed0b685085218882e7
ucn0925 <195886413+ucn0925@users.noreply.github.com>
2026-02-18 14:19:46 +0900

    行きたいボタンのUIを改善

--- app/controllers/shop_favorites_controller.rb
+++ app/controllers/shop_favorites_controller.rb
@@ -4,13 +4,13 @@ class ShopFavoritesController < ApplicationController
   def create
     shop = Shop.find(params[:shop_id]) # どのお店をお気に入りにするか探す
     current_user.shop_favorites.create(shop: shop) # ログイン中のユーザーでお気にいり登録
-    redirect_to request.referer || root_path, notice: "お気に入りに登録しました！" # 店舗ページに戻る
+    redirect_to request.referer || root_path
   end
 
   def destroy
     shop = Shop.find(params[:shop_id]) # 対象のお店を探す
     favorite = current_user.shop_favorites.find_by(shop_id: shop.id) # そのお店をお気に入りした記録を探す
     favorite&.destroy
-    redirect_to request.referer || root_path, notice: "お気に入りを解除しました！"
+    redirect_to request.referer || root_path
   end
 end
